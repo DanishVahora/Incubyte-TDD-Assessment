e074459490c6b62293697fae7dcb9024ff77dc9c
DanishVahora <danishvhora786@gmail.com>
2025-07-04 23:24:04 +0530

    [Step-1] refactor: simplify number parsing in Add method

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,14 +1,12 @@
 // Basic implementation of stringCalculator function
 
 function Add(input) {
-
     if (input === '') {
         return 0; // Return 0 for empty string
     }
 
-    const numbers = input.split(',').map(num => parseInt(num, 10));
+    const numbers = input.split(',').map(Number);
     return numbers.reduce((sum, num) => sum + num, 0); // Sum up all numbers
-
 }
 
 module.exports = Add;
