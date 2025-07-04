1c874289bf42f2215b697e5ab51f1b3d3426b1df
DanishVahora <danishvhora786@gmail.com>
2025-07-04 22:11:42 +0530

    [Init]        chore: project setup (npm init, jest, folders)

---
+++ tests/stringCalculator.test.js
@@ -0,0 +1,7 @@
+
+describe('String Calculator', () => {
+    // Test case: Verify that an empty string returns 0
+    test('should return 0 for an empty string', () => {
+        expect(true).toBe(true); 
+    });
+});
