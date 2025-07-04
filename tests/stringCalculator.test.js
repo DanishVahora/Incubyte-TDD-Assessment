f534697f47875bb5539cbfc1a96dd8b90adc2ba8
DanishVahora <danishvhora786@gmail.com>
2025-07-04 23:09:40 +0530

    [Step-1] test: add failing test for two numbers input

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,12 +1,15 @@
 const Add = require('../src/stringCalculator');
 
 describe('String Calculator', () => {
-
     test('should return 0 for an empty string', () => {
-        expect(Add('')).toBe(0); // Test case passes now
+        expect(Add('')).toBe(0);
     });
 
     test('should return a number for a single number input', () => {
-        expect(Add('1')).toBe(1); // Test case of single number input passes now
+        expect(Add('1')).toBe(1);
+    });
+
+    test('should return the sum for two numbers', () => {
+        expect(Add('1,2')).toBe(3); // Failing test case
     });
 });
