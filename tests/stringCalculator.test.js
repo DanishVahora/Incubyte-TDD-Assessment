201ffff316ff6783b6787b76fd17b0ab81233f4e
DanishVahora <danishvhora786@gmail.com>
2025-07-05 00:35:09 +0530

    [Step-3] test: add failing test for handling new lines between numbers

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -25,12 +25,17 @@ describe('String Calculator', () => {
     });
 
 
-    // Step-2: Failing test case for multiple numbers
+    // Step-2: Test case for multiple numbers
     test('should return the sum for multiple numbers', () => {
         expect(Add('1,2,3,4,5')).toBe(15);
         expect(Add('6,7,8,9,10')).toBe(40);
     });
 
+
+    // Step-3: Failing test case for new lines between numbers
+    test('should handle new lines between numbers', () => {
+        expect(Add('1\n2,3')).toBe(6); // Failing test case
+    });
 });
 
 
